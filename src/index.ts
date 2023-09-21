2dee5078fcff9c92e7243d70d8005afd01eadfac
huesoskywalker <agustinbigoni@gmail.com>
2023-09-21 13:08:34 -0300

    refactor: index for AWS lambda

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-// import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda"
+import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda"
+import { syncContacts } from "./helpers/sync/syncContacts"
+import { winstonLogger } from "./services/logger"
+import { searchOwnerContacts } from "./helpers/search/searchOwnerContacts"
 
-const handler = async (): // event: APIGatewayEvent,
-// context: Context,
-// callback: Callback
-Promise<void> => {
+const handler: Handler = async ({
+    event,
+    context,
+    callback,
+}: {
+    event: APIGatewayEvent
+    context: Context
+    callback: Callback
+}): Promise<void> => {
     try {
-        console.log(`start Sync`)
+        // In here pass the function we would like to sync or search for the dashboard metrics
+        // example:
         // await syncContacts()
-        // callback(null, {
-        //     statusCode: 200,
-        //     body: JSON.stringify({ message: "Success " }),
-        // })
+        // or:
+        // await searchOwnerContacts()
+
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Success " }),
+        })
     } catch (error: unknown) {
         if (error instanceof Error) {
-            // winstonLogger.error("Error in handler", {
-            //     error: error.message,
-            //     type: error.name,
-            //     stack: error.stack,
-            // })
-            // callback(error, null)
+            winstonLogger.error("Error in handler", {
+                error: error.message,
+                type: error.name,
+                stack: error.stack,
+            })
+            callback(error, null)
+        } else {
+            callback(new Error(`Unknown error occurred`), null)
         }
-        // else {
-        //     callback(new Error(`Unknown error occurred`), null)
-        // }
     }
 }
-
-handler()
-
-// module.exports = {
-//     handler,
-// }
